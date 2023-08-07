068c3f57be287769ea1ac3ff28836684d98ff7c6
kyyneogs <oks990304@gmail.com>
2023-08-07 21:45:01 +0900

    2023-08-07

--- stars-10997.py
+++ stars-10997.py
@@ -11,28 +11,29 @@
 # *********
 
 N = int(input())
-N = 1 + (N+1)*2
-arr = [[' ' for _ in range(N)] for _ in range(N)]
-
-def draw(x, y, max_x, max_y, flag_x=1, flag_y=0, flag_pm=1, cnt=1):
-    if max_x==0 or max_y==0:
-        return
-    
-    arr[x][y] = '*'
-    x, y, cnt = x + flag_pm*flag_x*1, y + flag_pm*flag_y*1, cnt+1
-
-    for i in arr:
-        print(''.join(i))
-    print('')
-
-    if cnt*flag_x+1==max_x or cnt*flag_y==max_y+1:
-        flag_x, flag_y = flag_y, flag_x
-        if cnt*flag_y+1==max_y: flag_pm = flag_pm * -1
-        max_x, max_y = max_x - 2*flag_x, max_y - 2*flag_y
-        cnt=1
-    draw(x, y, max_x, max_y, flag_x, flag_y, flag_pm, cnt)
+N = 1 + (N-1)*4
+arr = [[' ' for _ in range(N)] for _ in range((N+2) if N>1 else 1)]
 
 for i in range(N):
     arr[0][i] = '*'
 
-draw(1,0,10,8)
+x, y, max_x, max_y = 0, 0, N+1, N-1
+flag_x, flag_y, sign = 1, 0, 1
+cnt = 0
+
+while max_x>0 and N-1!=0:
+    arr[x][y] = '*'
+
+    if (cnt+1)*flag_x==max_x+1 or (cnt+1)*flag_y==max_y+1:
+        if (cnt+1)*flag_y==max_y+1: sign = sign * -1
+        max_x, max_y = max_x - 2*flag_x, max_y - 2*flag_y
+        flag_x, flag_y = flag_y, flag_x
+        cnt=0
+
+    x, y, cnt = x + flag_x*sign, y + flag_y*sign, cnt+1
+
+for i, j in enumerate(arr):
+    if i != 1:
+        print(''.join(j))
+    else:
+        print('*')
